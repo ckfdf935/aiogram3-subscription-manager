68b4e905daf141ff34033ab6e68a5343c2ed1308
Temshchik <131591415+ckfdf935@users.noreply.github.com>
2026-02-27 23:05:31 +0300

    Update models.py

--- models.py
+++ models.py
@@ -6,26 +6,26 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 
 Base = declarative_base()
 
-class User(Base): # таблица клиентов
+class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     telegram_id = Column(BigInteger, unique=True)
     username = Column(String, nullable=True)
     is_admin = Column(Boolean, default=False)
-    subscription_until = Column(DateTime, nullable=True) # дата и время, до которого действует подписка
+    subscription_until = Column(DateTime, nullable=True)
 
     payments = relationship("Payment", back_populates="user")
 
 
-class Payment(Base): # бухгалтерия
+class Payment(Base): 
     __tablename__ = "payments"
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id")) # Foreign Key (внешний ключ). Он связывает платеж с конкретным пользователем из первой таблицы
-    amount = Column(Integer) #Сумма
+    user_id = Column(Integer, ForeignKey("users.id")) 
+    amount = Column(Integer) 
     currency = Column(String)
-    provider_payment_id = Column(String) # ID транзакции от платежки
+    provider_payment_id = Column(String) 
     created_at = Column(DateTime, default=datetime.utcnow)
 
     user = relationship("User", back_populates="payments")
